5f151b42d88b1b3db37c6f548ca65f120f22a3e1
kaurkomal10 <kau14953@sheridancollege.ca>
2023-06-05 18:49:29 -0400

    Updated code

---
+++ src/practice1/Practice1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practice1;
+
+/**
+ *
+ * @author komal kaur
+ */
+public class Practice1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Komal kaur");
+        System.out.println("Table 2");
+        int i;
+        for (i=0 ; i<=10 ; i++) {
+            System.out.println("2"+ "X" + i + "=" + 2*i);
+        }
+    }
+}
